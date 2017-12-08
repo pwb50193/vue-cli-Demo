d41e2ceee8f784470281999ebfa974e63528374b
pwb50193 <1242739820@qq.com>
2017-12-08 10:47:41 +0800

    为导航添加点击事件

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -3,14 +3,14 @@
 	<div class="navi-container navi-horziontal" v-if='published=="horizontal"'>
 		<div class="hor-nav-one" v-for="nav in navigation">{{nav}}
 			<div class="hor-nav-bd">
-				<div class="hor-nav-two" v-for='item in list[nav]' >{{item}}</div>
+				<div @click='clickItem(item[1])' class="hor-nav-two" v-for='item in list[nav]' >{{item[0]}}</div>
 			</div>
 		</div>
 	</div>
 	<div class="navi-container navi-vertical" v-else>
 		<div class="ver-nav-one" v-for="nav in navigation">{{nav}}
 			<div class="ver-nav-bd">
-				<div class="ver-nav-two" v-for='item in list[nav]' >{{item}}</div>
+				<div @click='clickItem' class="ver-nav-two" v-for='item in list[nav]' >{{item[0]}}</div>
 			</div>
 		</div>
 	</div>
@@ -29,7 +29,9 @@
 			}
 		},
 		methods:{
-
+			clickItem:function(event){
+				this.$emit("click", event);
+			}
 		}
 	}
 </script>
@@ -67,6 +69,7 @@
 		width:100%;
 		height: 100%;
 		position: absolute;
+		z-index: 10000;
 		top: 100%;
 		left: 0;
 		display: none;
